d8068e8dc94366350bb2a18d98d687dfd1dcbf65
abrakadam <yehorslyvkin@gmail.com>
2026-05-26 00:50:21 +0200

    исправлена ошибка mixin apply error для getblockinteractionrange путем миксина в player

--- src/main/java/volthack/mixin/render/MixinPlayer.java
+++ src/main/java/volthack/mixin/render/MixinPlayer.java
@@ -1,26 +1,28 @@
 package volthack.mixin.render;
 
-import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import volthack.modules.player.MultiTask;
 
-@Mixin(LocalPlayer.class)
-public class MixinLocalPlayer {
-
+@Mixin(Player.class)
+public class MixinPlayer {
     @Inject(method = "getBlockInteractionRange", at = @At("HEAD"), cancellable = true)
     private void onGetBlockInteractionRange(CallbackInfoReturnable<Double> cir) {
-        if (volthack.modules.player.Reach.INSTANCE.getEnabled()) {
-            cir.setReturnValue((double) volthack.modules.player.Reach.INSTANCE.getBlockReach());
+        if ((Object)this instanceof net.minecraft.client.player.LocalPlayer) {
+            if (volthack.modules.player.Reach.INSTANCE.getEnabled()) {
+                cir.setReturnValue((double) volthack.modules.player.Reach.INSTANCE.getBlockReach());
+            }
         }
     }
 
     @Inject(method = "getEntityInteractionRange", at = @At("HEAD"), cancellable = true)
     private void onGetEntityInteractionRange(CallbackInfoReturnable<Double> cir) {
-        if (volthack.modules.player.Reach.INSTANCE.getEnabled()) {
-            cir.setReturnValue((double) volthack.modules.player.Reach.INSTANCE.getEntityReach());
+        if ((Object)this instanceof net.minecraft.client.player.LocalPlayer) {
+            if (volthack.modules.player.Reach.INSTANCE.getEnabled()) {
+                cir.setReturnValue((double) volthack.modules.player.Reach.INSTANCE.getEntityReach());
+            }
         }
     }
 }
