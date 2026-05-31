9677786dedd56f79c15c6dd4c20f3043c1518263
abrakadam <yehorslyvkin@gmail.com>
2026-05-31 16:28:57 +0200

    пофикшена подсветка для airplace и scaffold: исправлена позиция, добавлен setlinewidth, переписана система рендера

--- src/main/java/ravex/utility/render/BlockRenderer.java
+++ src/main/java/ravex/utility/render/BlockRenderer.java
@@ -15,7 +15,7 @@ public class BlockRenderer {
 
         // Bottom face edges
         renderLine(consumer, matrix, min, min, min, max, min, min, ir, ig, ib, ia);
-        renderLine(consumer, matrix, max, min, min, max, min, max, ir, ig, ig, ia); // correct colors
+        renderLine(consumer, matrix, max, min, min, max, min, max, ir, ig, ib, ia);
         renderLine(consumer, matrix, max, min, max, min, min, max, ir, ig, ib, ia);
         renderLine(consumer, matrix, min, min, max, min, min, min, ir, ig, ib, ia);
 
@@ -33,7 +33,54 @@ public class BlockRenderer {
     }
 
     private static void renderLine(VertexConsumer consumer, Matrix4f matrix, float x1, float y1, float z1, float x2, float y2, float z2, int r, int g, int b, int a) {
-        consumer.addVertex(matrix, x1, y1, z1).setColor(r, g, b, a).setNormal(x2 - x1, y2 - y1, z2 - z1);
-        consumer.addVertex(matrix, x2, y2, z2).setColor(r, g, b, a).setNormal(x2 - x1, y2 - y1, z2 - z1);
+        consumer.addVertex(matrix, x1, y1, z1).setColor(r, g, b, a).setNormal(x2 - x1, y2 - y1, z2 - z1).setLineWidth(1.0f);
+        consumer.addVertex(matrix, x2, y2, z2).setColor(r, g, b, a).setNormal(x2 - x1, y2 - y1, z2 - z1).setLineWidth(1.0f);
+    }
+
+    // For VertexFormat.Mode.QUADS (DEBUG_FILLED_BOX pipeline)
+    public static void renderFilledBoxQuads(VertexConsumer consumer, Matrix4f matrix, double size, float r, float g, float b, float a) {
+        float min = (float) ((1.0 - size) / 2.0);
+        float max = (float) ((1.0 + size) / 2.0);
+
+        int ir = (int)(r * 255);
+        int ig = (int)(g * 255);
+        int ib = (int)(b * 255);
+        int ia = (int)(a * 255);
+
+        // Bottom (y=min)
+        consumer.addVertex(matrix, min, min, min).setColor(ir, ig, ib, ia);
+        consumer.addVertex(matrix, max, min, min).setColor(ir, ig, ib, ia);
+        consumer.addVertex(matrix, max, min, max).setColor(ir, ig, ib, ia);
+        consumer.addVertex(matrix, min, min, max).setColor(ir, ig, ib, ia);
+
+        // Top (y=max)
+        consumer.addVertex(matrix, min, max, min).setColor(ir, ig, ib, ia);
+        consumer.addVertex(matrix, min, max, max).setColor(ir, ig, ib, ia);
+        consumer.addVertex(matrix, max, max, max).setColor(ir, ig, ib, ia);
+        consumer.addVertex(matrix, max, max, min).setColor(ir, ig, ib, ia);
+
+        // North (z=min)
+        consumer.addVertex(matrix, min, min, min).setColor(ir, ig, ib, ia);
+        consumer.addVertex(matrix, min, max, min).setColor(ir, ig, ib, ia);
+        consumer.addVertex(matrix, max, max, min).setColor(ir, ig, ib, ia);
+        consumer.addVertex(matrix, max, min, min).setColor(ir, ig, ib, ia);
+
+        // South (z=max)
+        consumer.addVertex(matrix, min, min, max).setColor(ir, ig, ib, ia);
+        consumer.addVertex(matrix, max, min, max).setColor(ir, ig, ib, ia);
+        consumer.addVertex(matrix, max, max, max).setColor(ir, ig, ib, ia);
+        consumer.addVertex(matrix, min, max, max).setColor(ir, ig, ib, ia);
+
+        // West (x=min)
+        consumer.addVertex(matrix, min, min, min).setColor(ir, ig, ib, ia);
+        consumer.addVertex(matrix, min, min, max).setColor(ir, ig, ib, ia);
+        consumer.addVertex(matrix, min, max, max).setColor(ir, ig, ib, ia);
+        consumer.addVertex(matrix, min, max, min).setColor(ir, ig, ib, ia);
+
+        // East (x=max)
+        consumer.addVertex(matrix, max, min, min).setColor(ir, ig, ib, ia);
+        consumer.addVertex(matrix, max, max, min).setColor(ir, ig, ib, ia);
+        consumer.addVertex(matrix, max, max, max).setColor(ir, ig, ib, ia);
+        consumer.addVertex(matrix, max, min, max).setColor(ir, ig, ib, ia);
     }
 }
--- src/main/java/ravex/utility/render/Render3DUtils.java
+++ src/main/java/ravex/utility/render/Render3DUtils.java
@@ -1,74 +1,29 @@
 package ravex.utility.render;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.MultiBufferSource;
-import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.blaze3d.vertex.BufferBuilder;
+import com.mojang.blaze3d.vertex.ByteBufferBuilder;
+import com.mojang.blaze3d.vertex.MeshData;
+import net.minecraft.client.renderer.rendertype.RenderType;
+import net.minecraft.client.renderer.rendertype.RenderTypes;
+import org.joml.Matrix4f;
 
 public class Render3DUtils {
-    public static double[] getCameraPos(net.minecraft.client.Camera camera) {
-        try {
-            java.lang.reflect.Field positionField = camera.getClass().getDeclaredField("position");
-            positionField.setAccessible(true);
-            net.minecraft.world.phys.Vec3 posVec = (net.minecraft.world.phys.Vec3) positionField.get(camera);
-            return new double[]{posVec.x, posVec.y, posVec.z};
-        } catch (Exception e) {
-            return new double[]{0.0, 0.0, 0.0};
-        }
+    private static final ByteBufferBuilder ALLOCATOR = new ByteBufferBuilder(RenderType.SMALL_BUFFER_SIZE);
+
+    private static final RenderType FILL_TYPE = RenderTypes.debugFilledBox();
+    private static final RenderType LINE_TYPE = RenderTypes.lines();
+
+    public static void renderFilledBox(Matrix4f matrix, double size, float r, float g, float b, float a) {
+        BufferBuilder builder = new BufferBuilder(ALLOCATOR, FILL_TYPE.mode(), FILL_TYPE.format());
+        BlockRenderer.renderFilledBoxQuads(builder, matrix, size, r, g, b, a);
+        MeshData mesh = builder.buildOrThrow();
+        FILL_TYPE.draw(mesh);
     }
 
-    public static float getPitch(net.minecraft.client.Camera camera) {
-        try {
-            java.lang.reflect.Field f = camera.getClass().getDeclaredField("xRot");
-            f.setAccessible(true);
-            return ((Number) f.get(camera)).floatValue();
-        } catch (Exception e) {
-            try {
-                java.lang.reflect.Field f = camera.getClass().getDeclaredField("pitch");
-                f.setAccessible(true);
-                return ((Number) f.get(camera)).floatValue();
-            } catch (Exception ex) {
-                return 0.0f;
-            }
-        }
-    }
-
-    public static float getYaw(net.minecraft.client.Camera camera) {
-        try {
-            java.lang.reflect.Field f = camera.getClass().getDeclaredField("yRot");
-            f.setAccessible(true);
-            return ((Number) f.get(camera)).floatValue();
-        } catch (Exception e) {
-            try {
-                java.lang.reflect.Field f = camera.getClass().getDeclaredField("yaw");
-                f.setAccessible(true);
-                return ((Number) f.get(camera)).floatValue();
-            } catch (Exception ex) {
-                return 0.0f;
-            }
-        }
-    }
-
-    public static VertexConsumer getLinesConsumer(MultiBufferSource.BufferSource bufferSource) {
-        try {
-            Class<?> renderTypeClass = Class.forName("net.minecraft.client.renderer.RenderType");
-            java.lang.reflect.Method linesMethod = renderTypeClass.getDeclaredMethod("lines");
-            Object linesRenderType = linesMethod.invoke(null);
-            
-            java.lang.reflect.Method getBufferMethod = bufferSource.getClass().getMethod("getBuffer", renderTypeClass);
-            return (VertexConsumer) getBufferMethod.invoke(bufferSource, linesRenderType);
-        } catch (Exception e) {
-            return null;
-        }
-    }
-
-    public static void endLinesBatch(MultiBufferSource.BufferSource bufferSource) {
-        try {
-            Class<?> renderTypeClass = Class.forName("net.minecraft.client.renderer.RenderType");
-            java.lang.reflect.Method linesMethod = renderTypeClass.getDeclaredMethod("lines");
-            Object linesRenderType = linesMethod.invoke(null);
-            
-            java.lang.reflect.Method endBatchMethod = bufferSource.getClass().getMethod("endBatch", renderTypeClass);
-            endBatchMethod.invoke(bufferSource, linesRenderType);
-        } catch (Exception ignored) {}
+    public static void renderWireframe(Matrix4f matrix, double size, float r, float g, float b, float a) {
+        BufferBuilder builder = new BufferBuilder(ALLOCATOR, LINE_TYPE.mode(), LINE_TYPE.format());
+        BlockRenderer.renderWireframe(builder, matrix, size, r, g, b, a);
+        MeshData mesh = builder.buildOrThrow();
+        LINE_TYPE.draw(mesh);
     }
 }
