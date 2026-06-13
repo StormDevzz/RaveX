eca5b96837287cc3b8057d8a9e68b459fb171c04
abrakadam <abrakadam@users.noreply.github.com>
2026-06-13 14:24:40 +0200

    Add @Mutable to packet accessors

--- src/main/java/ravex/mixin/network/AccessorServerboundMovePlayerPacket.java
+++ src/main/java/ravex/mixin/network/AccessorServerboundMovePlayerPacket.java
@@ -2,22 +2,26 @@ package ravex.mixin.network;
 
 import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(ServerboundMovePlayerPacket.class)
 public interface AccessorServerboundMovePlayerPacket {
+    @Mutable
     @Accessor("onGround")
     void setOnGround(boolean onGround);
 
     @Accessor("xRot")
     float getxRot();
 
+    @Mutable
     @Accessor("xRot")
     void setxRot(float xRot);
 
     @Accessor("yRot")
     float getyRot();
 
+    @Mutable
     @Accessor("yRot")
     void setyRot(float yRot);
 }
