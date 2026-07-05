3da30f2b1790d4bc611ce736e88a9037520b4870
abrakadam <abrakadam@users.noreply.github.com>
2026-07-05 15:15:20 +0200

    maybe now gradlew?

--- src/main/java/ravex/modules/combat/SelfTrap.java
+++ src/main/java/ravex/modules/combat/SelfTrap.java
@@ -26,7 +26,7 @@ import java.util.List;
 import java.util.Set;
 import ravex.utility.nativelib.NativeLibrary;
 public class SelfTrap extends Module {
-    public static final SelfTrap INSTANCE = new SelfTrap(); //Xiaomi
+    public static final SelfTrap INSTANCE = new SelfTrap(); //Xiaomii
     public final ActionParameter blocks = new ActionParameter("Blocks", () -> {
         Minecraft.getInstance().setScreen(new ravex.gui.blockbrowser.BlockBrowserScreen(
             Minecraft.getInstance().screen,
