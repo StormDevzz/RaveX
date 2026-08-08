39d148da3964d0702a7b182e90cf47e81e7a1ff7
sh2-u34r <sh2-u34r@proton.me>
2026-08-08 18:38:16 +0200

    feat: Fix Sodium Mixin

--- src/main/java/ravex/integrations/sodium/SodiumWorldRendererMixin.java
+++ src/main/java/ravex/integrations/sodium/SodiumWorldRendererMixin.java
@@ -3,22 +3,19 @@ package ravex.integrations.sodium;
 import net.caffeinemc.mods.sodium.client.render.SodiumWorldRenderer;
 import net.caffeinemc.mods.sodium.client.util.FogParameters;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 import ravex.modules.render.NoRender;
 import ravex.modules.Modules;
 
 @Mixin(SodiumWorldRenderer.class)
 public abstract class SodiumWorldRendererMixin {
-    @Unique
-    private static final FogParameters DISABLED_FOG = new FogParameters(0, 0, 0, 0, Float.MAX_VALUE, Float.MAX_VALUE, Float.MAX_VALUE, Float.MAX_VALUE);
 
-    @ModifyVariable(method = "setupTerrain", at = @At("HEAD"), argsOnly = true, name = "fogParameters")
-    private FogParameters ravex$modifyFogParameters(FogParameters fogParameters) {
+    @ModifyArgs(method = "setupTerrain", at = @At(value = "INVOKE", target = "Lnet/caffeinemc/mods/sodium/client/render/chunk/RenderSectionManager;update(Lnet/minecraft/client/Camera;Lnet/caffeinemc/mods/sodium/client/render/viewport/Viewport;Lnet/caffeinemc/mods/sodium/client/util/FogParameters;Z)V"))
+    private void ravex$modifyFogParameters(Args args) {
         if (Modules.enabled(NoRender.class) && Modules.get(NoRender.class).fog) {
-            return DISABLED_FOG;
+            args.set(2, FogParameters.NONE);
         }
-        return fogParameters;
     }
 }
